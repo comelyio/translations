cb12829e48bc189182fac6d36bf58b1e2ebdd3c1
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-12 21:37:56 +0500

    Cache on directory

---
+++ Cache/DiskCache.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * This file is part of Comely package.
+ * https://github.com/comelyio/comely
+ *
+ * Copyright (c) 2016-2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comelyio/comely/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Comely\IO\Translator\Cache;
+
+use Comely\IO\FileSystem\Disk\Directory;
+use Comely\IO\FileSystem\Exception\DiskException;
+use Comely\IO\Translator\Exception\CachedLanguageException;
+use Comely\IO\Translator\Languages\Language;
+
+/**
+ * Class DiskCache
+ * @package Comely\IO\Translator\Cache
+ */
+class DiskCache implements LanguageCacheInterface
+{
+    /** @var Directory */
+    private $directory;
+
+    /**
+     * DiskCache constructor.
+     * @param Directory $directory
+     */
+    public function __construct(Directory $directory)
+    {
+        $this->directory = $directory;
+    }
+
+    /**
+     * @param string $name
+     * @param string $group
+     * @return Language|null
+     * @throws CachedLanguageException
+     */
+    public function get(string $name, string $group): ?Language
+    {
+        try {
+            $cachedFile = $this->directory->file(sprintf('lang.%s.%s.php.cache', $name, $group));
+        } catch (DiskException $e) {
+            // File no exists, no error to give
+            return null;
+        }
+
+        // Cached file found, attempt to read
+        try {
+            $serialized = $cachedFile->read();
+        } catch (DiskException $e) {
+            throw new CachedLanguageException(
+                sprintf('Failed to read cached language file "%s"', basename($cachedFile->path()))
+            );
+        }
+
+        // Unserialize
+        $language = unserialize($serialized, [
+            "allowed_classes" => [
+                'Comely\IO\Translator\Languages\Language'
+            ]
+        ]);
+
+        if (!$language || !$language instanceof Language) {
+            throw new CachedLanguageException(
+                sprintf('Cached language file "%s" is incomplete or corrupted', basename($cachedFile->path()))
+            );
+        }
+
+        return $language;
+    }
+
+    /**
+     * @param Language $language
+     * @throws CachedLanguageException
+     */
+    public function store(Language $language): void
+    {
+        $cacheFile = sprintf('lang.%s.%s.php.cache', $language->name(), $language->group());
+        try {
+            $this->directory->write($cacheFile, serialize($language), false, true);
+        } catch (DiskException $e) {
+            throw new CachedLanguageException(
+                sprintf('Failed to store "%s" in cache directory', $cacheFile)
+            );
+        }
+    }
+}
--- Cache/LanguageCacheInterface.php
+++ Cache/LanguageCacheInterface.php
@@ -14,10 +14,26 @@ declare(strict_types=1);
 
 namespace Comely\IO\Translator\Cache;
 
+use Comely\IO\Translator\Exception\CachedLanguageException;
+use Comely\IO\Translator\Languages\Language;
+
 /**
  * Interface LanguageCacheInterface
  * @package Comely\IO\Translator\Cache
  */
 interface LanguageCacheInterface
 {
+    /**
+     * @param string $name
+     * @param string $group
+     * @return Language|null
+     * @throws CachedLanguageException
+     */
+    public function get(string $name, string $group): ?Language;
+
+    /**
+     * @param Language $language
+     * @throws CachedLanguageException
+     */
+    public function store(Language $language): void;
 }
